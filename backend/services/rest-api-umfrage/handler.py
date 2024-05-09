d614b680668060b3d32cb72c8de2485fef5b3d8a
Daniel <Daniel.kom0108@hotmail.com>
2024-05-09 14:03:07 +0200

    deleteUmfrageById hinzugefügt, noch nicht getestet.

--- backend/services/rest-api-umfrage/handler.py
+++ backend/services/rest-api-umfrage/handler.py
@@ -1,12 +1,51 @@
 import json
+from sqlalchemy.orm import sessionmaker
+
+from backend.models.models import Umfrage
+from backend.utils.database import create_local_engine
+
+engine = create_local_engine()
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+# Database connection with aws secrets manager
+# engine, Session = create_database_connection()
+
+def deleteUmfrageById(event, context):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        # Extrahiere die ID aus der URL
+        umfrage_id = event['pathParameters']['id']
+        # finde die Umfrage in der Datenbank
+        umfrage = session.query(Umfrage).filter_by(id=umfrage_id).first()
+
+        if umfrage:
+            # Lösche die Umfrage aus der Datenbank
+            session.delete(umfrage)
+            session.commit()
+            # Erstelle die Antwort
+            body = {
+                "message": f"Umfrage mit ID {umfrage_id} wurde erfolgreich gelöscht.",
+                "input": event,
+            }
+        else:
+            body = {
+                "message": f"Umfrage mit ID {umfrage_id} wurde nicht gefunden.",
+                "input": event,
+            }
+
+        response_status = 200
+    except Exception as e:
+        session.rollback()
+        body = {
+            "message": str(e)
+        }
+        response_status = 400
+    finally:
+        session.close()
+
+    response = {
+        "statusCode": response_status,
+        "body": json.dumps(body)
     }
-
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
     return response
