5db931e31049fedc516d11590b273d9240cd6bd1
Ahsen Sahan <92890437+AhsenSa@users.noreply.github.com>
2024-06-04 22:05:17 +0200

    delete account & change password

--- frontend/src/pages/password/forgotPassword/SetPassword.jsx
+++ frontend/src/pages/password/forgotPassword/SetPassword.jsx
@@ -1,34 +1,42 @@
-import { useEffect, useState } from 'react'
 import { Alert, Button, Form, Input } from 'antd'
-import { useLocation, useNavigate, Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { login } from '../../redux/actions/authActions.js'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { setPassword } from '../../../redux/actions/authActions.js'
 
-const Login = () => {
+const SetPassword = () => {
     const dispatch = useDispatch()
     const location = useLocation()
     const navigate = useNavigate()
     const [errorMessage, setErrorMessage] = useState('')
 
-    const { error, isAuthenticated } = useSelector((state) => state.adminLogin)
+    const { error, loading, success } = useSelector(
+        (state) => state.adminRegister
+    )
 
     const redirect = location.search ? location.search.split('=')[1] : '/'
 
     const handleSubmit = (values) => {
-        const { email, password } = values
-        dispatch(login({ email, password }))
+        console.log('values: ', values)
+        const { email, newPassword } = values
+        dispatch(setPassword({ email, newPassword }))
     }
 
     useEffect(() => {
-        if (isAuthenticated) {
-            navigate(redirect)
+        if (success) {
+            setTimeout(() => {
+                navigate({
+                    pathname: '/login'
+                })
+            }, 3000)
         }
         if (error) {
+            console.log('error: ', error)
             setErrorMessage(error)
         } else {
             setErrorMessage('')
         }
-    }, [isAuthenticated, error, navigate, redirect])
+    }, [success, redirect, navigate, error])
 
     return (
         <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
@@ -39,10 +47,10 @@ const Login = () => {
             ) : null}
             <div className="container mx-auto p-8 bg-white shadow-md rounded-lg max-w-md w-full">
                 <h1 className="text-2xl font-semibold text-center mb-6">
-                    Login
+                    Set Password
                 </h1>
                 <Form
-                    name="normal_login"
+                    name="normal_change_password"
                     initialValues={{
                         remember: true,
                     }}
@@ -67,81 +75,66 @@ const Login = () => {
                     >
                         <Input
                             type="email"
-                            placeholder="Email"
                             size="large"
                             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
                         />
                     </Form.Item>
-
                     <Form.Item
-                        name="password"
-                        label="Password"
+                        name="newPassword"
+                        label="New password"
                         rules={[
                             {
                                 required: true,
-                                message: 'Please input your password!',
+                                message: 'Please input your new password!',
                             },
                         ]}
                         className="w-full"
                     >
                         <Input.Password
-                            placeholder="Password"
                             size="large"
                             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
                         />
                     </Form.Item>
-
+                    <Form.Item
+                        name="confirmPassword"
+                        label="Confirm Password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please confirm your password!'
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (!value || getFieldValue('newPassword') === value) {
+                                        return Promise.resolve()
+                                    }
+                                    return Promise.reject(new Error('The two passwords that you entered do not match!'))
+                                }
+                            })
+                        ]}
+                        hasFeedback
+                        className="w-full"
+                    >
+                        <Input.Password
+                            size="large"
+                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                        />
+                    </Form.Item>
                     <Form.Item className="w-full">
                         <Button
                             type="primary"
                             htmlType="submit"
                             className="w-full py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                             size="large"
+                            disabled={loading}
                         >
-                            Einloggen
+                            Submit
                         </Button>
                     </Form.Item>
                 </Form>
-                <div className="text-center mt-4">
-                    <span>Noch kein Account? </span>
-                    <Link
-                        to="/register"
-                        className="text-indigo-600 hover:text-indigo-800 font-medium"
-                    >
-                        hier registrieren
-                    </Link>
-                </div>
-
-                <div className="text-center mt-4">
-                    <span>Passwort vergessen? </span>
-                    <Link
-                        to="/forgotPassword"
-                        className="text-red-600 hover:text-indigo-800 font-medium"
-                    >
-                        hier ändern
-                    </Link>
-                </div>
-                <div className="text-center mt-4">
-                    <span>Neues Passwort? </span>
-                    <Link
-                        to="/changePassword"
-                        className="text-green-600 hover:text-indigo-800 font-medium"
-                    >
-                        hier ändern
-                    </Link>
-                </div>
-                <div className="text-center mt-4">
-                    <span>Konto löschen? </span>
-                    <Link
-                        to="/deleteAccount"
-                        className="text-yellow-600 hover:text-indigo-800 font-medium"
-                    >
-                        hier löschen
-                    </Link>
-                </div>
             </div>
         </div>
     )
 }
 
-export default Login
+export default SetPassword
