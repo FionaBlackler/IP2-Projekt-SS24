27d296929b3adf3eca944564a53ec9860e4ccb16
serko64 <Serkankabak64@gmail.com>
2024-06-07 17:48:54 +0200

    fixes

--- frontend/src/routes/root.routes.jsx
+++ frontend/src/routes/root.routes.jsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Homepage from '../pages/home/Homepage.jsx'
 import Login from '../pages/login/Login.jsx'
 import ChangePassword from '../pages/password/changePassword/ChangePassword.jsx'
 import DeleteAccount from '../pages/admin/DeleteAccount.jsx'
 import ForgotPassword from '../pages/password/forgotPassword/ForgotPassword.jsx'
 import SetPassword from '../pages/password/forgotPassword/SetPassword.jsx'
 import Register from '../pages/register/Register.jsx'
-import ProtectedRoute from './protected.route.jsx'
-import Homepage from '../pages/home/Homepage.jsx'
 import Umfrage from '../pages/admin/meineUmfrage/Umfrage.jsx'
-import UmfragePopup from '../pages/admin/uploadUmfragePage/UmfragePopup.jsx'
 
 function RootRoutes() {
     return (
