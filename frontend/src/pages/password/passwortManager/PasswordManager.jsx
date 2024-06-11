0ed3ed83902f574889f00d4fbdf5934e5ab02353
Julian <Julan09@outlook.de>
2024-06-11 19:43:45 +0200

    Passoword Manager

--- frontend/src/pages/password/passwortManager/PasswordManager.jsx
+++ frontend/src/pages/password/passwortManager/PasswordManager.jsx
@@ -1,7 +1,7 @@
 import {useState} from "react";
+import {FaRegEye, FaRegEyeSlash} from "react-icons/fa";
 
 const PasswordManager = () => {
-
     const [currentPassword, setCurrentPassword] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
@@ -9,6 +9,12 @@ const PasswordManager = () => {
     const [isEmpty, setIsEmpty] = useState(false);
     const [showPopup, setShowPopup] = useState(false);
     const [showConfirmation, setShowConfirmation] = useState(false);
+
+    const [showPassword, setShowPassword] = useState({
+        currentPassword: false,
+        password: false,
+        confirmPassword: false
+    });
 
     const handleCurrentPasswordChange = (e) => {
         setCurrentPassword(e.target.value);
@@ -32,6 +38,13 @@ const PasswordManager = () => {
         }
     };
 
+    const toggleShowPassword = (field) => {
+        setShowPassword(prevState => ({
+            ...prevState,
+            [field]: !prevState[field]
+        }));
+    };
+
     const handleClick = (e) => {
         if (currentPassword === '' || password === '' || confirmPassword === '') {
             setIsEmpty(true);
@@ -39,7 +52,6 @@ const PasswordManager = () => {
             setIsEmpty(false);
             setShowPopup(true);
         }
-
     }
 
     const handleClose = () => {
@@ -52,34 +64,52 @@ const PasswordManager = () => {
 
     return (
         <div className="flex bg-[#AF8A74] w-screen h-screen justify-center items-center">
-            <div className="shadow-lg bg-[#FAEEDB] rounded-lg py-16 px-32 text-center">
+            <div className="shadow-lg bg-[#FAEEDB] rounded-lg py-16 px-32 text-center relative">
                 <span className="ml-4 mt-4 font-semibold">
                     <h1 className="text-2xl">Password Manager</h1>
                 <table className="mt-12">
                     <tr>
-                        <td className="px-6"><input type="password"
-                                                    placeholder="Aktuelles Passwort"
-                                                    value={currentPassword}
-                                                    onChange={handleCurrentPasswordChange}
-                                                    className="border-[1px] border-black rounded-lg h-12 w-72 placeholder-gray-400 text-center text-black"/>
-                        </td>
-                        </tr>
-                    <br/>
-                    <tr>
-                        <td className="px-6"><input type="password"
-                                                    placeholder="Neues Passwort"
-                                                    value={password}
-                                                    onChange={handlePasswordChange}
-                                                    className={`border-[1px] rounded-lg h-12 w-72 ${passwordsMatch ? 'border-green-600' : 'border-red-500'} text-center p-2`}/>
+                        <td className="px-6 relative">
+                            <input type={showPassword.currentPassword ? 'text' : 'password'}
+                                   placeholder="Aktuelles Passwort"
+                                   value={currentPassword}
+                                   onChange={handleCurrentPasswordChange}
+                                   className="border-[1px] border-black rounded-lg h-12 w-72 placeholder-gray-400 text-center text-black pr-10"/>
+                            <div onClick={() => toggleShowPassword('currentPassword')}
+                                 className="absolute inset-y-0 right-0 pr-11 flex items-center cursor-pointer">
+                                {showPassword.currentPassword ? <FaRegEyeSlash className="h-5 w-5 text-gray-500"/> :
+                                    <FaRegEye className="h-5 w-5 text-gray-500"/>}
+                            </div>
                         </td>
                     </tr>
                     <br/>
                     <tr>
-                        <td className="px-6"><input type="password"
-                                                    placeholder="Passwort Bestätigen"
-                                                    value={confirmPassword}
-                                                    onChange={handleConfirmPasswordChange}
-                                                    className={`border-[1px] rounded-lg h-12 w-72 ${passwordsMatch ? 'border-green-600' : 'border-red-500'} text-center p-2`}/>
+                        <td className="px-6 relative">
+                            <input type={showPassword.password ? 'text' : 'password'}
+                                   placeholder="Neues Passwort"
+                                   value={password}
+                                   onChange={handlePasswordChange}
+                                   className={`border-[1px] rounded-lg h-12 w-72 ${passwordsMatch ? 'border-green-600' : 'border-red-500'} text-center p-2 pr-10`}/>
+                            <div onClick={() => toggleShowPassword('password')}
+                                 className="absolute inset-y-0 right-0 pr-11 flex items-center cursor-pointer">
+                                {showPassword.password ? <FaRegEyeSlash className="h-5 w-5 text-gray-500"/> :
+                                    <FaRegEye className="h-5 w-5 text-gray-500"/>}
+                            </div>
+                        </td>
+                    </tr>
+                    <br/>
+                    <tr>
+                        <td className="px-6 relative">
+                            <input type={showPassword.confirmPassword ? 'text' : 'password'}
+                                   placeholder="Passwort Bestätigen"
+                                   value={confirmPassword}
+                                   onChange={handleConfirmPasswordChange}
+                                   className={`border-[1px] rounded-lg h-12 w-72 ${passwordsMatch ? 'border-green-600' : 'border-red-500'} text-center p-2 pr-10`}/>
+                            <div onClick={() => toggleShowPassword('confirmPassword')}
+                                 className="absolute inset-y-0 right-0 pr-11 flex items-center cursor-pointer">
+                                {showPassword.confirmPassword ? <FaRegEyeSlash className="h-5 w-5 text-gray-500"/> :
+                                    <FaRegEye className="h-5 w-5 text-gray-500"/>}
+                            </div>
                             {!passwordsMatch && <p className="text-red-500">Password do not match</p>}
                             {isEmpty && <p className="text-red-500">Bitte füllen sie alle felder aus</p>}
                         </td>
@@ -93,27 +123,34 @@ const PasswordManager = () => {
                             className="ml-11 mt-6 text-white bg-[#222] py-2 px-6 rounded-lg hover:bg-green-500 hover:text-white duration-300"
                         >Speichern</button>
                     {showPopup && (
-                        <div className="mt-4 shadow-lg bg-white rounded-lg py-6 px-6 text-center">
-                            <h2>Sind Sie sich sicher?</h2>
-                            <button onClick={() => {
-                                setShowPopup(false);
-                                setShowConfirmation(true);
-                            }}
-                                    className="mt-4 text-white bg-[#210803] py-2 px-6 rounded-lg hover:bg-green-500 hover:text-white duration-300">Bestätigen
-                            </button>
+                        <div
+                            className="shadow-lg bg-white rounded-lg py-6 px-6 text-center absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50"> {/* Hinzufügen von 'bg-black bg-opacity-50' hier */}
+                            <div className="bg-white bg-opacity-50 p-6 rounded-lg">
+                                <h2>Sind Sie sich sicher?</h2>
+                                <button onClick={() => {
+                                    setShowPopup(false);
+                                    setShowConfirmation(true);
+                                }}
+                                        className="mt-4 text-white bg-[#210803] py-2 px-6 rounded-lg hover:bg-green-500 hover:text-white duration-300">Bestätigen
+                                </button>
+                            </div>
                         </div>
                     )}
                     {showConfirmation && (
-                        <div className="mt-4 shadow-lg bg-white rounded-lg py-16 px-16 text-center">
-                            <h2 className="font-semibold text-green-500">Erfolgreich geändert</h2>
-                            <button onClick={handleClose}
-                                    className="mt-4 text-white bg-[#210803] py-2 px-6 rounded-lg hover:bg-red-500 hover:text-white duration-300">Schließen
-                            </button>
+                        <div
+                            className="shadow-lg bg-white rounded-lg py-16 px-16 text-center absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50"> {/* Hinzufügen von 'bg-black bg-opacity-50' hier */}
+                            <div
+                                className="bg-white bg-opacity-50 p-6 rounded-lg">
+                                <h2 className="font-semibold text-black">Erfolgreich geändert</h2>
+                                <button onClick={handleClose}
+                                        className="mt-4 text-white bg-[#210803] py-2 px-6 rounded-lg hover:bg-red-500 hover:text-white duration-300">Schließen
+                                </button>
+                            </div>
                         </div>
                     )}
                     </span>
             </div>
         </div>
     )
-}
+};
 export default PasswordManager;
