0315cdfed431557ea17ae2c80d5a1c98044d2db3
bberb <b.berber@live.de>
2024-06-11 13:55:42 +0200

    change login and register page style

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./index.html', './src/**/*.{js,jsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'primary-color': '#FEF2DE',
+                'secondary-color': '#AF8A74',
+                'accent-color': '#73594b',
+            },
+        },
+    },
+    plugins: [],
+}
