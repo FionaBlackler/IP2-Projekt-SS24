ed7f5a0aeff82222d8b097eea6e5b6377cc45d24
Serkan Kabak <12kase1bif@hft-stuttgart.de>
2024-06-14 11:48:32 +0000

    fixes

--- frontend/src/pages/admin/meineUmfrage/Umfrage.jsx
+++ frontend/src/pages/admin/meineUmfrage/Umfrage.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import UmfragenTable from './UmfragenTable'
 import ArchiveTable from './ArchiveTable'
 import UmfragePopup from '../uploadUmfragePage/UmfragePopup'
-import Popup from 'E:/IF_6/umfragetool/frontend/src/components/Popup.jsx'
+import Popup from '../../../components/Popup.jsx'
 
 export default function Umfrage() {
     const [data, setData] = useState({ umfragen: [] })
